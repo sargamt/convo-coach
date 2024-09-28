1cf65baee5d09c3472103c5ccd5fe96b2e3fd7c0
Sargam Thakur <110199481+sargamt@users.noreply.github.com>
2024-09-28 18:08:34 -0400

    set up Google Gemini API

--- backend/speech_to_text.py
+++ backend/speech_to_text.py
@@ -1,44 +1,68 @@
-import speech_recognition as sr
+import google.generativeai as genai
+import os
 
-# Initialize the recognizer
-r = sr.Recognizer()
+# set up API key
+genai.configure(api_key=os.environ["GOOGLE_GEM_API_KEY"])
 
-def record_text():
-    # Loop in case of errors
-    while(1):
-        try:
-            # Use the mic as a source for input
-            with sr.Microphone() as source2:
-                # Prepare recognizer to recieve input
-                r.adjust_for_ambient_noise(source2, duration=0.2)
+# set up Gemini model
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-                # Listens for the user's input
-                audio2 = r.listen(source2)
+# upload audio file
+myfile = genai.upload_file("New_Recording_12.mp3")
+# print(f"{myfile=}")
 
-                # Using google to recognize audio
-                MyText = r.recognize_google_cloud(audio2)
+prompt1 = "Can you evaluate how confident he sounds?"
+prompt2 = "Give it a rating on a scale of 0 to 100 percent."
 
-                return MyText
+textEval = model.generate_content([myfile, prompt1])
+ratingEval = model.generate_content([myfile, prompt2])
+
+print(textEval.text)
+print(ratingEval.text)
+
+# this works^^^
+
+# import speech_recognition as sr
+
+# # Initialize the recognizer
+# r = sr.Recognizer()
+
+# def record_text():
+#     # Loop in case of errors
+#     while(1):
+#         try:
+#             # Use the mic as a source for input
+#             with sr.Microphone() as source2:
+#                 # Prepare recognizer to recieve input
+#                 r.adjust_for_ambient_noise(source2, duration=0.2)
+
+#                 # Listens for the user's input
+#                 audio2 = r.listen(source2)
+
+#                 # Using google to recognize audio
+#                 MyText = r.recognize_google_cloud(audio2)
+
+#                 return MyText
             
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
+#         except sr.RequestError as e:
+#             print("Could not request results; {0}".format(e))
 
-        except sr.UnknownValueError:
-            print("Unknown Error Occurred")
+#         except sr.UnknownValueError:
+#             print("Unknown Error Occurred")
 
-    return
+#     return
 
-def output_text(text):
-    # Access output.txt file from this directory and makes one if it does not exist
-    # "a" is for append so it does not overwrite the text every time
-    f = open("output.txt", "a")
-    f.write(text)
-    f.write("\n")
-    f.close
-    return
+# def output_text(text):
+#     # Access output.txt file from this directory and makes one if it does not exist
+#     # "a" is for append so it does not overwrite the text every time
+#     f = open("output.txt", "a")
+#     f.write(text)
+#     f.write("\n")
+#     f.close
+#     return
 
-while(1):
-    text = record_text()
-    output_text(text)
+# while(1):
+#     text = record_text()
+#     output_text(text)
 
-    print("Wrote text")
+#     print("Wrote text")
